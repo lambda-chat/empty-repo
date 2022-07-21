617c240aff39d43d4270ca173a69adda05a91d4c
lambda-chat <chat.algebrique@gmail.com>
2022-07-21 22:53:48 +0900

    Revert "USERNAMEはイケてなかったのでUSER_NAMEに名前を変えた"
    This reverts commit de6a942322871ec140ee9f055bf78b0d5ef236c6.

--- service.py
+++ service.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     load_dotenv()
     print(".env ファイルから環境変数を読み込みました")
-    print(os.environ["USER_NAME"])
+    print(os.environ["USERNAME"])
     print(os.environ["PASSWORD"])
